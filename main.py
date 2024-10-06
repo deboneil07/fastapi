d0e376e5a5e1cc2fabe9db898fabccd84756ab37
deboneil07 <bhattacharjeedeboneil@gmail.com>
2024-10-07 00:09:18 +0530

    basic url shortener setup

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
-from typing import Union
-from fastapi import FastAPI
+#from typing import Union
+import secrets
+from fastapi import FastAPI, HTTPException, Depends
+from sqlalchemy.orm import Session
+import validators
+
+import schemas, models
+from database import SessionLocal, engine
 
 app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def raise_bad_request(message):
+    raise HTTPException(status_code=400, detail=message)
 
 @app.get('/')
 def read_root():
-    return {"message" : "hello world!"}
+    return "Welcome to url shortener API :D"
 
-@app.get('/items/{item_id}')
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"response" : item_id, "q" : q}
+@app.post('/url', response_model=schemas.URLInfo)
+def create_url(url: schemas.URLBase):
+    if not validators.url(url.target_url):
+        raise_bad_request(message="provided url is not valid")
+
+    return f"TODO: create db entry for: {url.target_url}"
